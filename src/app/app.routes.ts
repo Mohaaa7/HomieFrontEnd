3cd396bd568eebf2680fe45e2c179f078d746571
Moha Hamra <mohah3000@gmail.com>
2025-11-28 13:41:10 +0100

    Creadas componentes restantes con sus rutas por terminar de diseñar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
-import { FormularioViviendaComponent } from './components/formulario-vivienda/formulario-vivienda.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { GuardadosComponent } from './components/guardados/guardados.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
-    { path: 'formulario', component: FormularioViviendaComponent},
     { path: 'login', component: LoginComponent},
     { path: 'registro', component: RegistroComponent},
+    { path: 'formulario', component: FormularioComponent},
+    { path: 'guardados', component: GuardadosComponent}
 ];
---
+++ src/app/components/guardados/guardados.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guardados',
+  imports: [],
+  templateUrl: './guardados.component.html',
+  styleUrl: './guardados.component.css'
+})
+export class GuardadosComponent {
+
+}
