653eeaa6791544779f16b1a7054a59a4352789ca
Moha Hamra <mohah3000@gmail.com>
2025-11-28 13:32:44 +0100

    Diseño del login y del registro creados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { FormularioViviendaComponent } from './components/formulario-vivienda/formulario-vivienda.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'formulario', component: FormularioViviendaComponent},
-    { path: 'login', component: LoginComponent}
+    { path: 'login', component: LoginComponent},
+    { path: 'registro', component: RegistroComponent},
 ];
