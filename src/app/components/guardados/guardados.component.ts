145b5b09e812a85617cf3c616b730473bd64d7e4
Moha Hamra <mohah3000@gmail.com>
2025-12-03 13:46:54 +0100

    Mejoras realizadas en la componente guardados

--- src/app/components/guardados/guardados.component.ts
+++ src/app/components/guardados/guardados.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-guardados',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './guardados.component.html',
   styleUrl: './guardados.component.css'
 })
