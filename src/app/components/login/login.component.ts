baf42cf4510e972f1a088922416587679c82b8cc
Moha Hamra <mohah3000@gmail.com>
2025-12-04 12:08:25 +0100

    Lógica del typescript en el lógin hecha y añadido la ruta login en el servidor

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup;
+  mensaje = '';
+
+  constructor(private fb: FormBuilder, private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    if (this.loginForm.invalid) {
+      this.mensaje = 'Por favor completa todos los campos';
+      return;
+    }
+
+    const {username, password} = this.loginForm.value
+
+    this.apiService.login(username, password).subscribe({
+      next: (res) => {
+        if(res.success) {
+          this.mensaje = 'Login exitoso!';
+          localStorage.setItem('token', res.token!);
+        } else {
+          this.mensaje = res.mensaje || 'Login fallido';
+        }
+      },
+      error: (err) => {
+        console.error(err);
+        this.mensaje = 'Error al conectar con el servidor';
+      }
+    });
+  }
+
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,11 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private backendUrl = 'http://localhost:8000';
+  private API_URL = 'http://localhost:5000/api';
 
   constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(`${this.API_URL}/login`, {username, password});
+  }
+
 }
